a6edc2b45552fe6a19b608a8508bcabf36b7b133
Rahul Borate <91531299+rahul-borate@users.noreply.github.com>
2021-09-30 14:54:23 +0530

    Update dse-handlers.spec.ts

--- test/dse-handlers.spec.ts
+++ test/dse-handlers.spec.ts
@@ -31,36 +31,10 @@ const context: Context = {
 // source configuration
 const config = {
     apiUrl: 'https://demo.docusign.net',
-    accountId: '14072015',
-    clientId: 'df45eb70-ba0f-4106-9e0f-ce83fdb4d4a9',
-    userId: '34bade63-d863-4d12-a40d-5acd5a11fed0',
-    privateKey: `-----BEGIN RSA PRIVATE KEY-----
-MIIEowIBAAKCAQEAgxhIk1YpLE9JLnuEcUEfulDULGkYT4qGzRJdRCIdsLiEgkD4
-d3nIfQwYzdN6pwh4wMOiN8EmBwxXCXSbd7OtBSw3maJSpvH6ceQCSkoIZFNBvYKG
-a1deIilhA8Y/c+IZYdZQxIBC8aNoO0zHlI9Y/sSeaisRjyQE8Ovj3Vb9PRfcCZkz
-m+fB3yMGSZqCLO0QN4gE5SPt1PotM9BDnmMeADfLu0CvmsP4tJ01fgLXAyjsbghY
-1nySxTjPRWgNafXVe6nXN4SCn9VnVKsmCCQ/LNyFSIa3CZeBicCV3ngq+Fy7/opm
-sfLNrhKbAyCR0NDzuq7jvGFT81k5lTF/UCZndwIDAQABAoIBAG+ORYwjYMwU+Ch2
-8YBSs8YfrD3SfN6K+iUeQIGPKobMre4NjaJV3tcSaTUVhiUHn4IhuDQLx+lthc9F
-DFszacOiGfpB2GW6Oq2aIpzHHRcKhkHXBb3oTfFyFt1YYXyiZYkNEBP3NYBc1oDH
-2HpB9bBu7oNX/Qw60X2WroQW106ZbtSy+VlawyECoSGriZ6hXiGKpjU4EK0Eu9Mi
-qyvX08Z0ysOukxnZK3ElVEFpHSXgvGwYSsSBPTmtGdja8jvBC3dCulyYxvLjBmiT
-sUZD+P4JaccscgxRNdA9BZBy131BAGx5ZbvfArFjKMpuk2w82h7zjEw/8f8zrG1O
-jPG2myECgYEA9a8NhunJORrtcM+qLNdvoiHd3/NQeftng4LKYdDfZYhpfGOAtvPY
-5P3oOLdUmoeWf9DW4qBZTiluQP+jhu0fnbrfwSHwj83HGGGUBH8uA+GZTWtRdzYh
-aEhcc0DCvUHdIOJ4/KfDqmYaL+7e+iF40NH2m53xjiOiR3VzOQQUAacCgYEAiJl4
-oQME8y5pv4XOcPxLbtCeDMNhInUwXr/0AqYqeyc4LlqvqHvlaUIeDMBzJYM3KEOo
-01wtlp/+mCRa+V8++47o3J7hMVHjcmJpKoOSVb5eYVgyVC5HzeJaX1b+BoQ3y7Sk
-sx389mnAZYuGpWIpH/L9cY6Z3X4Ntu0/1iKPBbECgYEAiZ0tAYXQHXl0yDy5wHZH
-5FP9+KRMTpdAawyGmfWqPDAFjhQaE+BQfCuwuJwP+qK57QwoYTaZ70sLo8LNcRU/
-IVV8YazGF3aW75Qwmbg3KRtjiFb1gcXDPqtRhYkfy2yqFPJl/HdoBPk9791n7klz
-Z9xDuvTkSc6x5j7mdGhRYH8CgYAz85KCQiC20DcdxSkPpZSWz7bcic4+ia1cy8pk
-24eakP73+juGa6CDmMbO+L6STMZEhP8Ha65W9ACjImiLISNWDzMW/UIOPOOxFCaF
-KccU3zML+LwjcfqTHfxa+D3igpSp38kn6kbT/MJUnG0YJy4W3ZZwgtiX+aIDqVW2
-/ivA8QKBgBn+AGns2YIA4GyabYBDTG+yvVCCc4jpHHSA12y0r53uUFMpIzQovok+
-vGNZu35lvP4F6w9OD6BqE5zBMezh8c+epOyoGigREVddfP8bWTFZjOS1kdnKQgB7
-zfkEG1WlCiKaKZAg/+ADmzkTEkwfYpn9oJWVP6zBgWbjSDwBjtFB
------END RSA PRIVATE KEY-----`
+    accountId: '14082017',
+    clientId: 'df45eb70-ba0f-67491-9e0f-ce83fdb4d4a9',
+    userId: '34bade631-d8635-4d12-a40d-5acd5a11fed0',
+    privateKey: `-----BEGIN RSA PRIVATE KEY----------END RSA PRIVATE KEY-----`
 }
 
 // set config in node process env
