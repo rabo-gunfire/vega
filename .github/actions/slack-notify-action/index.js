fa96853694375a02a4f5edbcc4c842222bfd538a
Rahul Borate <91531299+rahul-borate@users.noreply.github.com>
2021-10-05 09:19:30 +0530

    Create index.js

---
+++ .github/actions/slack-notify-action/index.js
@@ -0,0 +1,40 @@
+const core = require("@actions/core");
+const fetch = require("node-fetch");
+
+(aysnc () => {
+  try {
+    const payload = {
+      channel: `${core.getInput("channel")}`,
+      attachments: [
+        {
+          color: status === "success" ? "#2e993e" : status === "failure" ? "#bd0f26" : "#d29d0c",
+          blocks: [
+            {
+              type: "section",
+              text: {
+                type: "mrkdwn",
+                text: `Github Action: *${status === "success" ? "SUCCESS" : status === "failure" ? "FAILURE" : "CANCELLED"}*`,
+              },
+            },
+          ],
+        },
+      ],
+    };
+
+    const rest = await fetch("https://slack.com/api/chat.postMessage", {
+      method: "POST",
+      body: JSON.stringify(payload),
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+        "Content-Length": payload.length,
+        Authorization: `Bearer ${core.getInput("slack-bot-token")}`,
+        Accept: "application/json",
+      },
+    });
+    if (!res.ok) {
+      throw new Error(`Server error ${res.status}`);
+    }
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+})();
